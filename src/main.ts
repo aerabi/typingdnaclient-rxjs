84d2f745ff9e485b5f60bfb6b704c3e38c7e90b9
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-02-12 04:10:28 +0100

    Write first lines of the wrapper TypingDNAReactiveClient

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+import { TypingDNAClient } from 'typingdnaclient';
+
+export class TypingDNAReactiveClient {
+    private readonly _client: TypingDNAClient;
+    constructor(private apiKey: string, private apiSecret: string, private apiServer: string) {
+        this._client = new TypingDNAClient(apiKey, apiSecret, apiServer);
+    }
+}
