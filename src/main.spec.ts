9291d77a13d4e346ded50e73ce5ece819fc9a3e6
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-02-12 07:18:49 +0100

    Feat: Add auto, save, and verify

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,8 +1,38 @@
-import { TypingDNAReactiveClient } from "./main";
+import { TypingDNAReactiveClient } from './main';
 
 describe('TypingDNAReactiveClient', () => {
-  it('constructor', () => {
-    const  client = new TypingDNAReactiveClient('', '', '');
+  let client: TypingDNAReactiveClient;
+
+  const userId = 'aerabi';
+  const typingPattern =
+    '0,3.1,0,0,22,3482580928,0,-1,-1,0,-1,-1,5,116,23,2,173,118,2,184,71,1,0,0,1,3,1,902248182,1,1,0,0,0,1,1920,1080,2,0,88,0,946510664|4135,116|189,55|184,95|73,102|119,103|80,168|96,80|72,131|105,55|79,111|89,103|104,95|183,111|97,103|104,118|112,95|152,142|120,79|6,154|290,87|224,55|128,72';
+
+  beforeAll(() => {
+    const apiKey = process.env.TYPING_DNA_API_KEY || '';
+    const apiSecret = process.env.TYPING_DNA_API_SECRET || '';
+    client = new TypingDNAReactiveClient(apiKey, apiSecret);
     expect(client).toBeTruthy();
+    expect(client['_client']).toBeTruthy();
+  });
+
+  it('auto', (done) => {
+    client.auto(userId, typingPattern).subscribe((response) => {
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
+  });
+
+  it('save', (done) => {
+    client.save(userId, typingPattern).subscribe((response) => {
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
+  });
+
+  it('verify', (done) => {
+    client.verify(userId, typingPattern).subscribe((response) => {
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,41 @@
-// @ts-ignore
-import { TypingDNAClient } from 'typingdnaclient';
+import { Observable, Subscriber } from 'rxjs';
+
+function bindSubscriber(subscriber: Subscriber<any>) {
+  return (error: any, response: any) => {
+    if (error) {
+      subscriber.error(error);
+    } else {
+      subscriber.next(response);
+    }
+    subscriber.complete();
+  };
+}
 
 export class TypingDNAReactiveClient {
-  private readonly _client: TypingDNAClient;
-  constructor(private apiKey: string, private apiSecret: string, private apiServer: string) {
+  private readonly _client: any;
+  constructor(private apiKey: string, private apiSecret: string, private apiServer: string = 'api.typingdna.com') {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const TypingDNAClient = require('typingdnaclient');
     this._client = new TypingDNAClient(apiKey, apiSecret, apiServer);
+  }
+
+  public auto(userId: string, typingPattern: string, options?: { customField: any }): Observable<any> {
+    if (options) {
+      return new Observable((observer) => this._client.auto(userId, typingPattern, options, bindSubscriber(observer)));
+    }
+    return new Observable((observer) => this._client.auto(userId, typingPattern, bindSubscriber(observer)));
+  }
+
+  public save(userId: string, typingPattern: string): Observable<any> {
+    return new Observable((observer) => this._client.save(userId, typingPattern, bindSubscriber(observer)));
+  }
+
+  public verify(
+    userId: string,
+    typingPattern: string,
+    quality = 2,
+    options: { deviceSimilarityOnly: boolean } = { deviceSimilarityOnly: false },
+  ): Observable<any> {
+    return new Observable((observer) => this._client.verify(userId, typingPattern, quality, options, bindSubscriber(observer)));
   }
 }
