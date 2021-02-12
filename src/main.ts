d5d7c198780f6e1e90efde9f8397db13e65b54b7
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-02-12 04:13:58 +0100

    Add prettier and reformat

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 import { TypingDNAClient } from 'typingdnaclient';
 
 export class TypingDNAReactiveClient {
-    private readonly _client: TypingDNAClient;
-    constructor(private apiKey: string, private apiSecret: string, private apiServer: string) {
-        this._client = new TypingDNAClient(apiKey, apiSecret, apiServer);
-    }
+  private readonly _client: TypingDNAClient;
+  constructor(private apiKey: string, private apiSecret: string, private apiServer: string) {
+    this._client = new TypingDNAClient(apiKey, apiSecret, apiServer);
+  }
 }
